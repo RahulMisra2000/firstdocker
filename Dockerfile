a80d7898ad8d7595168e24bae5b554349947e8c8
rahul <rahulmisra2000@gmail.com>
2021-01-23 01:19:13 -0500

    arg and env

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,23 @@ FROM node
 WORKDIR /app
 COPY ./package.json .
 RUN npm install
+
+# Just to see what got installed
+RUN ls -alt
 COPY ./server.js ./server.js
-EXPOSE 3000
+
+# Just to see if it copied over the server.js
+RUN ls -alt
+COPY ./pages ./pages/
+
+# Just to see if it copied over the pages folder
+RUN ls -alt
+
+# This is the default port. 
+# Can of course be overridden in the .env file which we supply to the docker run which spins up the container
+# It will be available in the environment of the container. In case of node, in process.env.PORT 
+ARG DEFAULT_PORT=3000
+ENV PORT $DEFAULT_PORT
+
+EXPOSE $PORT
 CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const path = require("path");
+const { writeFileSync, readFileSync } = require("fs");
+
 let firstName;
 console.log("start of server.js top level code");
 
@@ -11,31 +13,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.get("/writetovolume", (req, res) => {
+  writeFileSync(
+    path.join(__dirname, "data", "file.txt"),
+    new Date().toTimeString()
+  );
+  res.redirect("/");
+});
+
+app.get("/readvolumecontent", (req, res) => {
+  res.send(
+    readFileSync(path.join(__dirname, "data", "file.txt"), {
+      encoding: "utf8",
+      flag: "r",
+    })
+  );
+});
+
 app.get("/", (req, res) => {
-  res.send(`<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Test- Updated1</title>
-    </head>
-    <body>
-    You had entered ${firstName}
-      <form enctype="application/x-www-form-urlencoded" method="post">
-        <label for="firstName">Enter First Name:</label>
-        <input type="text" name="firstName" />
-        <button type="submit">Save</button>
-      </form>
-    </body>
-  </html>`);
+  res.sendFile(path.join(__dirname, "pages", "home.html"));
 });
 
 app.post("/", (req, res) => {
   console.log(req.body);
   firstName = req.body.firstName;
+  console.log(`You entered ${firstName}`);
   res.redirect("/");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
+
+// docker build -t image1:1.0 --build-arg DEFAULT_PORT=3000 .
+// docker run -dp 8000:3000 -v anyname:/app/data   -v "%cd%:/app"  -v /app/node_modules	--env-file ./.env --name cont1 image1:1.0
+
+// docker stop cont1
+// docker rm cont1
+// docker rmi image1:1.0
